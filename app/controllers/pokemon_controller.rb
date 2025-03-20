f7be1ff3a434999a08e55e0074ff06d1427330b5
cokaluk <cokaluk@academic.rrc.ca>
2025-03-19 21:15:03 -0500

    added a search bar in the nav bar

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -1,6 +1,11 @@
 class PokemonController < ApplicationController
   def index
-    @pokemons = Pokemon.includes(:types).page(params[:page]).per(20)
+    if params[:search].present?
+      @pokemons = Pokemon.where("name Like ?", "%#{params[:search]}%").page(params[:page]).per(20)
+    else
+      @pokemons = Pokemon.includes(:types).page(params[:page]).per(20)
+    end
+
   end
 
   def show
